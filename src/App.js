cb36313855cb39cdaabdf973c5fe702bf4bf7228
Vanik.Vardanyan <v.vardanyan@agimagroup.ru>
2021-01-28 12:41:05 +0400

    feature/ESP_237_TADAM

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 /* eslint-disable react/jsx-filename-extension */
-import React from 'react';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
-import Home from './components/home';
-import Bascet from './components/bascet';
+import React from "react";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+import Home from "./components/home";
+import Bascet from "./components/bascet";
 
 function App() {
   return (
     <BrowserRouter>
+      hallo world
       <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/bascet" component={Bascet} />
+        <Route path='/' exact component={Home} />
+        <Route path='/bascet' component={Bascet} />
       </Switch>
     </BrowserRouter>
   );
